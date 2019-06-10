4e275a079d403d8e9e2ebfba7e593da2b6c31e16
Talgat <talgat.s@protonmail.com>
2019-06-10 09:05:58 +0000

    Add login route and test github oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ require "http"
 
 class SessionsController < ApplicationController
   def new
-    puts('NEW!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
     args = { client_id: ENV['CLIENT_ID'] }
     redirect_to 'https://github.com/login/oauth/authorize?' + args.to_query
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "sessions/new"
+  get 'login', to: 'sessions#new'
   get 'sessions/create'
   get 'sessions/destroy'
   root 'static_pages#home'
