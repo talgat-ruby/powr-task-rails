72261b2dd46ca7f1a0f822912de0cb5df5d07aa9
Talgat <talgat.s@protonmail.com>
2019-06-10 19:31:04 +0000

    Add auth login feature

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,58 +1,55 @@
+require 'net/http'
+require 'net/https'
+require 'json'
+
 class AuthController < ApplicationController
-  @@access_token = ''
+  def initialize
+    @new_user = true
+  end
   
   def login
     begin
-      res = fetch_access_token()
+      token_data = fetch_access_token()
       
-      if token_res.kind_of? Net::HTTPSuccess
-        response_data = JSON.parse(token_res.body)
-        if response_data['error'].nil?
-          access_token = response_data['access_token']
-          user_res = fetch_user(access_token)
-          # redirect_to('/auth/success')
-        else
-          puts response_data['error']
-        end
-        puts("Success")
-      elsif
-        puts("Error")
+      if token_data['error'].nil?
+        access_token = token_data['access_token']
+        user_data = fetch_user_data(access_token)
+        save_user(user_data, access_token)
+      else
+        raise token_data['error']
       end
-    rescue => e
-      puts "failed #{e}"
-    end
-  end
-  
-  def fetch_access_token()
-    begin
-      uri = URI('https://github.com/login/oauth/access_token')
-      body = {
-        client_id: ENV['CLIENT_ID'],
-        client_secret: ENV['CLIENT_SECRET'],
-        code: params[:code]
-      }
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      req = Net::HTTP::Post.new(uri.path, {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      })
-      req.body = body.to_json
-      res = http.request(req)
       
-      if res.kind_of? Net::HTTPSuccess
-        response_data = JSON.parse(res.body)
-        return response_data
-      elsif
-        raise res.body 
-      end
     rescue => e
       # move to fail
       puts "failed #{e}"
     end
   end
   
-  def fetch_user(access_token)
+  def fetch_access_token()
+    uri = URI('https://github.com/login/oauth/access_token')
+    body = {
+      client_id: ENV['CLIENT_ID'],
+      client_secret: ENV['CLIENT_SECRET'],
+      code: params[:code]
+    }
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path, {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    })
+    req.body = body.to_json
+    res = http.request(req)
+    
+    if res.kind_of? Net::HTTPSuccess
+      response_data = JSON.parse(res.body)
+      return response_data
+    elsif
+      raise res.body 
+    end
+  end
+  
+  def fetch_user_data(access_token)
     uri = URI('https://api.github.com/user')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -61,25 +58,37 @@ class AuthController < ApplicationController
       'Accept': 'application/json',
       'Authorization': " token #{access_token}"
     })
-    return http.request(req)
-  end
-
-  def success
+    res = http.request(req)
     
-    user = User.new(
-      uid: data['id'],
+    if res.kind_of? Net::HTTPSuccess
+      response_data = JSON.parse(res.body)
+      return response_data
+    elsif
+      raise res.body 
+    end
+  end
+  
+  def save_user(data, access_token)
+    user = User.find_or_initialize_by(uid: data['id'])
+    if !user['id'].nil?
+      @new_user = false
+    end
+    user.update_attributes!(
       username: data['login'],
       avatar_url: data['avatar_url'],
       email: data['email'],
-      oauth_token: @@access_token
+      oauth_token: access_token
     )
+    
     if user.valid?
-      puts('VALID')
-      puts user
-      user.save
       @user = user
-      puts @user
+      user.save
+    else
+      raise user.errors.full_messages
     end
+  end
+
+  def success
   end
 
   def fail
